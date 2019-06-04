8a2c1408dd7afc9a8e7c74f4ec7a87060d552d74
unknown <swlee2@3ibank.com>
2019-06-04 17:50:18 +0900

    File Update

---
+++ src/Routes/Auth/AuthContainer.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import AuthPresenter from "./AuthPresenter";
+import useInput from "../../Hooks/useInput";
+import { useMutation } from "react-apollo-hooks";
+import { LOG_IN, CREATE_ACCOUNT, CONFIRM_SECRET } from "./AuthQueries";
+import { toast } from "react-toastify";
+
+export default () => {
+  const [action, setAction] = useState("logIn");
+  const username = useInput("");
+  const firstName = useInput("");
+  const lastName = useInput("");
+  const secret = useInput("");
+  const email = useInput("swy9112@naver.com");
+  const requestSecretMutation = useMutation(LOG_IN, {
+    variables: { email: email.value }
+  });
+
+  const createAccountMutation = useMutation(CREATE_ACCOUNT, {
+    variables: {
+      username: username.value,
+      email: email.value,
+      firstName: firstName.value,
+      lastName: lastName.value
+    }
+  });
+
+  const confirmSecretMutation = useMutation(CONFIRM_SECRET, {
+      variables: {
+          secret: secret.value
+      }
+  })
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    if (action === "logIn") {
+      if (email.value !== "") {
+        try {
+          const {
+            data: { requestSecret }
+          } = await requestSecretMutation();
+          console.log(requestSecret);
+          if (!requestSecret) {
+            toast.error("You dont have an account yet, create one");
+            setTimeout(() => setAction("signUp"), 3000);
+          } else {
+              toast.success("Check your inbox for your");
+              setAction("confirm");
+          }
+        } catch {
+          toast.error("Can't request secret, try again");
+        }
+      } else {
+        toast.error("Email is required");
+      }
+    } else if (action === "signUp") {
+      if (
+        email.value !== "" &&
+        username.value !== "" &&
+        firstName.value !== "" &&
+        lastName.value !== ""
+      ) {
+        try {
+          const {
+            data: { createAccount }
+          } = await createAccountMutation();
+          if (!createAccount) {
+            toast.error("Can't create account");
+          } else {
+            toast.success("Account created! Log In now");
+            setTimeout(() => setAction("logIn"), 3000);
+          }
+        } catch (e) {
+          toast.error(e.message);
+        }
+      } else {
+        toast.error("All field are required");
+      }
+    } else if(action === "confirm") {
+
+    }
+  };
+
+  return (
+    <AuthPresenter
+      setAction={setAction}
+      action={action}
+      username={username}
+      firstName={firstName}
+      lastName={lastName}
+      email={email}
+      secret={secret}
+      onSubmit={onSubmit}
+    />
+  );
+};
