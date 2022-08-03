c841011330ad93018ebf3d2a2c173d19f3072991
seungwoo lee2 <seungwoo_lee2@tmax.co.kr>
2022-08-03 22:26:07 +0900

    update

--- src/Routes/Profile/ProfileContainer.js
+++ src/Routes/Profile/ProfileContainer.js
@@ -1,17 +1,7 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import { useQuery, useMutation } from "react-apollo-hooks";
-import { GET_USER, LOG_OUT } from "./ProfileQueries";
 import ProfilePresenter from "./ProfilePresenter";
 
-export default withRouter(
-  ({
-    match: {
-      params: { username }
-    }
-  }) => {
-    //const { data, loading } = useQuery(GET_USER, { variables: { username } });
-    //const logUserOut = useMutation(LOG_OUT);
-    return <ProfilePresenter />;
-  }
-);
+export default withRouter(() => {
+  return <ProfilePresenter />;
+});
