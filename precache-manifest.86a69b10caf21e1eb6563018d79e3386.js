09f099440388ecd2d0f2fd95e063c488b9d8bb55
seungwoo lee2 <seungwoo_lee2@tmax.co.kr>
2022-08-03 22:23:17 +0900

    Updates

---
+++ precache-manifest.86a69b10caf21e1eb6563018d79e3386.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f952dac8b4547f35c2cfc2608d3eb73",
+    "url": "/prismagram-frontend/index.html"
+  },
+  {
+    "revision": "00e47f56ae310c640880",
+    "url": "/prismagram-frontend/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "00e47f56ae310c640880",
+    "url": "/prismagram-frontend/static/js/2.d7e73db4.chunk.js"
+  },
+  {
+    "revision": "b51bdb30d7719ec84bb5",
+    "url": "/prismagram-frontend/static/js/main.901f7072.chunk.js"
+  },
+  {
+    "revision": "47398cb4aa123f6b6fc5",
+    "url": "/prismagram-frontend/static/js/runtime~main.3356f13c.js"
+  }
+]);
